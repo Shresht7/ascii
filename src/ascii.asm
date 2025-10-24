55c252b6ca44a0e9bee86d87dcfb7bb63cbc48ea
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-25 00:10:24 +0530

    Implement conversion logic: `ASCII -> Decimal`

--- src/ascii.asm
+++ src/ascii.asm
@@ -42,6 +42,15 @@ section .data
     usage_msg db "Usage: ascii <char>", 0xA
     usage_msg_len equ $ - usage_msg
 
+    newline db 0xA
+
+; ------------------
+; UNINITIALIZED DATA
+; ------------------
+
+section .bss
+    buf resb 16             ; Reserve 16 bytes for buffer for decimal digits (enough for 0..255)
+
 ; ----
 ; CODE
 ; ----
@@ -56,6 +65,62 @@ _start:
     mov rax, [rsp]          ; Move argc value into rax
     cmp rax, 2              ; Compare argc with 2 (program_name + first_argument)
     jl .usage               ; less than 2 => Insufficient Arguments. Show Usage message 
+
+    ; Retrieve the first command-line argument argv[1]
+    ; argv[0] contains the program-name and is at [rsp + 8] (8 bits offset from rsp (argc))
+    ; argv[1] contains the first arugment (if it exists) and is at [rsp + 16] (8 * 2)
+    mov rsi, [rsp+16]       ; argv[1]
+    mov al, [rsi]           ; first character (pointer to rsi)
+    movzx rbx, al           ; zero-extend into rbx (value 0..255)
+
+    ; Prepare buffer pointer - fill from end
+    lea rdi, [buf+15]       ; rdi will point to end; we'll decrement before writing digits
+    xor rcx, rcx            ; digit count. Zero out for start
+
+    ; handle value == 0. specially (print "0")
+    cmp rbx, 0
+    jne .dec_loop
+
+    mov byte [rdi], '0'
+    dec rdi
+    inc rcx
+    jmp .after_conversion
+
+.dec_loop:
+    ; Standard division loop:
+    ; rbx = value
+    ; we want (quotient, remainder) = value / 10
+    mov rax, rbx        
+    xor rdx, rdx        ; Zero out rdx
+    mov r10, 10
+    div r10             ; quotient -> rax, remainder -> rdx
+    add dl, '0'         ; Ascii offset for 0 (as a string)
+    dec rdi
+    mov [rdi], dl
+    mov rbx, rax        ; new value = quotient
+    inc rcx
+
+    test rax, rax
+    jnz .dec_loop
+
+.after_conversion:
+    ; At this point:
+    ; rdi -> pointer to first digit (we decremented before storing)
+    ; rcx -> number of digits
+
+    ; Save values before write macro clobbers them
+    mov rsi, rdi        ; buffer pointer
+    mov rdx, rcx        ; byte count
+
+
+    ; Write decimal string
+    mov rax, SYS_WRITE
+    mov rdi, FD_STDOUT
+    ; rsi and rdx are already set
+    syscall
+
+    ; Write newline
+    write FD_STDOUT, newline, 1
 
     ; Exit with Success Status Code
     exit EXIT_SUCCESS
