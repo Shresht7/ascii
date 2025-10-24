cf9dfc9e28346d45e0b1dd505c12e0c039b904d9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-24 23:21:31 +0530

    Create `write` and `print` macros

--- src/ascii.asm
+++ src/ascii.asm
@@ -4,15 +4,30 @@
 ; DEFINITIONS
 ; -----------
 
+%define SYS_WRITE   1
 %define SYS_EXIT    60
+
+%define FD_STDOUT   1
 
 ; ------
 ; MACROS
 ; ------
 
+%macro write 3
+    mov rax, SYS_WRITE  ; syscall: write
+    mov rdi, %1         ; file-descriptor
+    mov rsi, %2         ; buffer to write
+    mov rdx, %3         ; number of bytes to write
+    syscall
+%endmacro
+
+%macro print 1
+    write FD_STDOUT, %1, %1_len
+%endmacro
+
 %macro exit 1
     mov rax, SYS_EXIT   ; syscall: exit
-    mov rdi, $1         ; exit status code
+    mov rdi, %1         ; exit status code
     syscall
 %endmacro
 
@@ -32,10 +47,5 @@ section .text
     global _start
 
 _start:
-    mov rax, 1                      ; syscall: write
-    mov rdi, 1                      ; file-descriptor: stdout
-    lea rsi, [rel hello_world]      ; buffer to write
-    mov rdx, hello_world_len        ; the number of bytes to write
-    syscall
-
+    print hello_world
     exit 0
